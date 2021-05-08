7588c7154a628d38218cb80c6d3c41e43665d8e2
Berkin Akaydın <berkin.akaydin@getir.com>
2021-05-08 19:35:57 +0300

    fix : for running in heroku

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const REGION = process.env.REGION || 'eu-west-1'; // The AWS Region. For example
 const BUCKET_NAME = 'berkinakaydin-affinidi-study';
 
 module.exports = {
-  HOST: process.env.HOST || 'localhost',
+  HOST: process.env.HOST || (process.env.NODE_ENV === 'production') ? undefined : 'localhost',
   PORT: process.env.PORT || 8080,
   REGION,
   BUCKET_NAME,
